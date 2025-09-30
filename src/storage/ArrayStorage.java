4c60bdeb63d7d956f475d5ede57f4e73a4a50258
tomafoks <tomafoks@mail.ru>
2025-10-01 00:36:11 +0500

    rafactor code in methods

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -7,28 +7,50 @@ import model.Resume;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     public void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     public void save(Resume r) {
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        }
     }
 
     public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                System.arraycopy(storage, i+1, storage, i, size-i-1);
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return new Resume[0];
+        var newResume = new Resume[size];
+        System.arraycopy(storage, 0, newResume, 0, size);
+        return newResume;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 }
