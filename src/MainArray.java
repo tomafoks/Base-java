0c8e4ae1eda80c0ae8cb07f3d1b9822ae4d3f7f1
tomafoks <tomafoks@mail.ru>
2025-09-30 23:33:19 +0500

    add new packages

--- src/MainArray.java
+++ src/MainArray.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import model.Resume;
+import storage.ArrayStorage;
+
 /**
  * Interactive test for ArrayStorage implementation
  * (just run, no need to understand)
@@ -32,7 +35,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -1,30 +1,34 @@
+package storage;
+
+import model.Resume;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
-    void clear() {
+    public void clear() {
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return new Resume[0];
     }
 
-    int size() {
+    public int size() {
         return 0;
     }
 }
