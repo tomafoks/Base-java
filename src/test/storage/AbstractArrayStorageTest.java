437601ec2ae23a7aa08f46bf30cac80c63d345b7
tomafoks <tomafoks@mail.ru>
2025-11-03 01:18:11 +0500

    test(storage):add new tests class from storages

---
+++ src/test/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,31 @@
+package test.storage;
+import org.junit.jupiter.api.Test;
+class AbstractArrayStorageTest {
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void size() {
+    }
+}
